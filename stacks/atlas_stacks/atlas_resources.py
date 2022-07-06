17597b6cd5375c1e28eb91110fe13e59b1cbf4a0
Thomas Von Heill <tomvonheill@gmail.com>
2022-07-06 12:28:06 -0700

    removing docker image asset again

--- stacks/atlas_stacks/atlas_resources.py
+++ stacks/atlas_stacks/atlas_resources.py
@@ -16,11 +16,9 @@ from aws_cdk import aws_lambda as aws_lambda
 from aws_cdk import aws_logs as aws_logs
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_ssm as ssm
-from aws_cdk.aws_ecr_assets import DockerImageAsset
 from aws_cdk.aws_applicationautoscaling import Schedule
 import aws_cdk
 from constructs import Construct
-from pathlib import Path
 
 
 class FargateService(Stack):
@@ -40,7 +38,6 @@ class FargateService(Stack):
             vpc=importedVPC)
         repository = ecr.Repository(self, "Repository",
             repository_name=f"{props['environment'].lower()}-eventbridge-atlas-repo")
-        image = DockerImageAsset(self,"EventBridgeAtlasImage",directory= str(Path.cwd().parent.parent))
         s3bucket = s3.Bucket(
             self,
             f"{props['project']}-{props['environment']}-bucket",
@@ -52,7 +49,7 @@ class FargateService(Stack):
             schedule=Schedule.cron(day='*',month='*',hour='*', minute='0'),
             cluster=cluster,
             scheduled_fargate_task_image_options=ecs_patterns.ScheduledFargateTaskImageOptions(
-                image=image,
+                image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
                 environment={
                     "EVENT_BUS_NAME": f"{props['environment']}-EventCentral",
                     "SCHEMA_REGISTRY_NAME": "discovered-schemas",
@@ -63,11 +60,9 @@ class FargateService(Stack):
         )
         fargate_service = ecs.FargateService(self, 'EventBridgeAtlasFargateService', task_definition=fargate_task.task_definition,cluster=cluster)
 
-        s3bucket.grant_read_write(fargate_task.task_definition.execution_role)
         self.output_props = props.copy()
         self.output_props['ecr_repo'] = repository
         self.output_props['ecs_service']=fargate_service
-        self.output_props['image']=image
 
     # pass objects to another stack
     @property
